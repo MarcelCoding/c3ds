fabfd7ae2e30c5cbeed67411281e9e00f7ea2e04
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-09-03 21:22:30 +0200

    Randomize Datenspuren Text

--- src/c3ds/core/templatetags/ds_utils.py
+++ src/c3ds/core/templatetags/ds_utils.py
@@ -1,3 +1,6 @@
+import itertools
+import random
+
 from django import template
 
 register = template.Library()
@@ -6,3 +9,15 @@ register = template.Library()
 @register.filter(name='times')
 def times(number):
     return range(number)
+
+
+_logo_view_counter = itertools.count(1)
+
+
+@register.simple_tag(name='logo_text')
+def logo_text():
+    """Return the logo text; every 10th render picks a funny typo."""
+    count = next(_logo_view_counter)
+    if count % 10 == 0:
+        return random.choice(['Datensuren', 'Dasputenren', 'Datensputen'])
+    return 'Datenspuren'
